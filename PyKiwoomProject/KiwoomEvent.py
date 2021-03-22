9ed00574b2cc23f37fc6d2f3433e7387ae8bebda
20190511 <baeghmath@soonsil.ac.kr>
2021-03-22 21:09:42 +0900

    실시간 데이터 수집 이벤트 제작중

--- PyKiwoomProject/KiwoomEvent.py
+++ PyKiwoomProject/KiwoomEvent.py
@@ -1,7 +1,9 @@
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QAxContainer import *
+from PyQt5.QtCore import *
 from pykiwoom.kiwoom import *
+from operator import *
 import os
 import pandas as pd
 import datetime
@@ -10,12 +12,20 @@ import time
 class KiwoomEvent():
     def __init__(self):
         self.today = self.today_date()
+        self.__data_path = "D:/PycharmProject/Test/PyKiwoomProject/dayData"
+
+        """ 멤버 변수 """
+        self.merge_filename = []
         self.code_company_date = []
+        self.sorted_code_company_date = [] #상장일 순으로 정렬된 Code 리스트
         self.code_list = []
+        self.test_aggregate = "empty"
 
         #로그인
         self.kiwoom = Kiwoom()
+        self.kiwoom.ocx.OnReceiveRealData.connect(self._handler_real_data)
         self.kiwoom.CommConnect(block=True)     #block는 자동으로 True 처리 (Blocking 처리)
+
 
         #오늘 날짜 얻기
     def today_date(self):
@@ -30,19 +40,6 @@ class KiwoomEvent():
             print("연결됨.")
         elif err_code == 0:
             print("연결되지 않음.")
-
-    """ 실시간 데이터 사용시 
-        실시간 데이터는 SetRealReg()함수로 연결 -> 데이터전달 -> DisConnectRealData()로 연결 끊기
-        이후 GetCommRealData로 데이터 받아옴.
-    """
-        # 실시간 데이터 연결 -> 2021_03-21 제작중,
-    def real_data(self, code_arr, fid_num):
-        self.kiwoom.SetRealReg("0101")
-
-        self.kiwoom.SetRealRemove("0101")
-
-
-    """ 실시간 데이터 사용시"""
 
         #전 종목의 코드 받아오기 (block_request함수를 이용해서 한꺼번에 데이터를 받아오는 것이 가능)
     def all_chart_code_extract(self):
@@ -60,26 +57,32 @@ class KiwoomEvent():
                                                    output="주식일봉차트조회",
                                                    next=0)
             out_name = f"{code}.xlsx"
-            dataframe_.to_excel("D:/PycharmProject/Test/PyKiwoomProject/dayData", sheet_name=out_name)
+            dataframe_.to_excel(self.__data_path, sheet_name=out_name)
             time.sleep(3.6)
 
         # 엑셀파일에서 데이터 추출.
     def all_chart_code_intract(self):
-
-        file_list = os.listdir("D:/PycharmProject/Test/PyKiwoomProject/dayData")
+        file_list = os.listdir(self.__data_path)     #데이터 경로 설정.
         xlst_list = [num for num in file_list if num.endswith(".xlsx")]
         close_data = []
+        about_data = input("데이터 종류 ex) 고가 or 저가 or 현재가 : ")
 
-            # 종가로 데이터 추출.
-        for xls in xlst_list:
+            # (종가)로 데이터 추출.
+        for index_num, xls in enumerate (xlst_list):
+            print("현재 작업 파일 = ", f"{index_num+1}/{len(xlst_list)}")
             code = xls.split(".")[0]
-            data_frame_ = pd.read_excel(xls)
-            data_frame2_ = data_frame_[["일자", "현재가"]].copy()
+            data_name = self.__data_path+"/"+xls
+            data_frame_ = pd.read_excel(data_name)
+            data_frame2_ = data_frame_[["일자", about_data]].copy()
+            data_frame2_.rename(columns={about_data: code}, inplace=True)
             data_frame2_ = data_frame2_.set_index("일자")
             data_frame2_ = data_frame2_[::-1]
             close_data.append(data_frame2_)
+
         data_frame_ = pd.concat(close_data, axis=1)
-        data_frame2_.to_excel("merge.xlsx")
+        file_name = "merge("f"{self.today}of{about_data}).xlsx"
+        self.merge_filename.append(file_name)
+        data_frame_.to_excel(file_name)
 
     """
         <Err_code>
@@ -132,27 +135,97 @@ class KiwoomEvent():
         self.code_list = all_chart      #모든 코드 차트 저장
         code_len = len(all_chart)
 
-        for code, index_num in enumerate(all_chart):
+        for index_num, code in enumerate(all_chart):
             temp_list = []
             company_name = self.kiwoom.GetMasterCodeName(code)
-            company_date = self.kiwoom.GetMasterListedStockDate(code)
+            company_date = self.kiwoom.GetMasterListedStockDate_original(code)
             temp_list.append(code)
             temp_list.append(company_name)
             temp_list.append(company_date)
             self.code_company_date.append(temp_list)
+        self.sorted_code_company_date = sorted(self.code_company_date, key=itemgetter(2))  #상장일 순으로 정렬
 
 
+    """ 실시간 데이터 사용시 -> 현재 사용불가 
+        실시간 데이터는 SetRealReg()함수로 연결 -> 데이터전달 -> DisConnectRealData()로 연결 끊기
+        이후 GetCommRealData로 데이터 받아옴.
+    """
+        #실시간 이벤트 처리 핸들러
+    def _handler_real_data(self, market_code, real_type, real_data):
+        if real_type == "시가총액":
+            aggregate_value = self.kiwoom.GetCommRealData(market_code, 311)
+
+        print("handler")
+        try:
+            self.real_loop.exit()
+        except AttributeError:
+            pass
+
+        self.test_aggregate = aggregate_value
 
 
+    """
+        (str) screen_no : 실시간 등록할 화면 번호
+        (str) code_list : 실시간 등록할 종목 코드 (; 구분자로 전달)
+        (str) fid_list  : 실시간 등록할 FID (":FID1;FID2:FID3;...")
+        (str) real_type : "0", "1" 타입
+            -> "0" : 같은 화면에서 다른 종목 실시간 등록을 할 때 앞의 항목이 자동 "연결해제"됨
+            -> "1" : 같은 화면에서 다른 종목 실시간 등록을 할 때 기존 종목도 실시간 시세를 받을 수 있음.
+                !- 꼭 같은 화면이어야 하고 최초 등록은 "0"으로 하고 이후부터 "1"이 가능함 -!
+    """
+    # 실시간 데이터 연결 -> 2021_03-21 제작중,
+    def real_data_open(self, code_arr, fid_list, real_type):
+        self.kiwoom.SetRealReg("1000", code_arr, fid_list, real_type)
+        print("real_data_open")
+        self.real_loop = QEventLoop()
+        self.real_loop.exec_()
+
+    """
+        screen_no : 실시간 해제할 화면 번호
+        del_code  : 실시간 해제할 종목 코드.
+    """
+    # 실시간 데이터 연결해제
+    def real_data_close(self, screen_no, del_code):
+        self.SetRealRemove("1000", screen_no, del_code)
+    """ 실시간 데이터 사용시"""
 
 
+        #실시간 시가총액 데이터 불러오기.
+    def _aggregate_value_company(self):
+        self._basic_data_()   #선 리스트 제작.
+        code_list = ";".join(self.code_list)
+        fid_list = list("311")
+        """
+        data_num = len(self.code_list) - 1
+        for i in range(data_num):
+            fid_list.append(";311")
+        """
+        real_type = "0"
+
+        self.kiwoom.SetRealReg("0101", code_list, fid_list, real_type)
+
+        print("check_point")
+        aggregate_list = []
+        for index, code in enumerate(self.code_list):
+            data = self.kiwoom.GetCommRealData(code, "311")
+            aggregate_list.append(data)
+            print(index," : ", data)
+
+        self.kiwoom.SetRealRemove("ALL", "ALL")
 
 
+    def real_test(self):
+        test_code = "039490"
+        test_fid = "311"
+        real_type = "0"
 
-
+        self.real_data_open(test_code, test_fid, real_type)
+        print(self.test_aggregate)
 
 
 
 kiwooms = KiwoomEvent()
     #주의 all_chart_code는 3시간가량 소요됨
 #kiwooms.all_chart_code()
+#kiwooms.all_chart_code_intract()
+kiwooms.real_test()
